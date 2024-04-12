45ebcd6adb85cc4b8b0b771a024263a5725657e8
damnhatanh123 <83380756+damnhatanh123@users.noreply.github.com>
2024-04-12 21:46:36 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import BT1 from './src/BT1';
+import BT2 from './src/BT2';
+import BT3 from './src/BT3';
+import BT4 from './src/BT4';
+import BT5 from './src/BT5';
+import BT6 from './src/BT6';
+import BT7 from './src/BT7';
+import BT8 from './src/BT8';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+const App = ()=>
+{
+    return (
+        <BT1/>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
